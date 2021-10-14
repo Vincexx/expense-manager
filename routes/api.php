a7e8ba9fa39453e66832fa5f5b3e007d4f535efc
= <=>
2021-10-14 21:53:00 +0800

    updates 0.4

--- routes/api.php
+++ routes/api.php
@@ -20,10 +20,8 @@ Route::middleware('auth:sanctum')->get('/user', function () {
     return response()->json(Auth::user());
 });
 
-// Route::post('login', 'api\LoginController@login');
-// Route::middleware('auth:sanctum')->post('logout', 'api\LoginController@logout');
 
-Route::prefix('user')->group(function() {
+Route::middleware('auth:sanctum')->prefix('user')->group(function() {
     Route::get('/list', 'api\UserController@list');
     Route::post('/store', 'api\UserController@store');
     Route::patch('/update/{user}', 'api\UserController@update');
@@ -37,14 +35,14 @@ Route::middleware('auth:sanctum')->prefix('role')->group(function() {
     Route::delete('/delete/{role}', 'api\RoleController@destroy');
 });
 
-Route::prefix('category')->group(function() {
+Route::middleware('auth:sanctum')->prefix('category')->group(function() {
     Route::get('/list', 'api\CategoryController@list');
     Route::post('/store', 'api\CategoryController@store');
     Route::patch('/update/{category}', 'api\CategoryController@update');
     Route::delete('/delete/{category}', 'api\CategoryController@destroy');
 });
 
-Route::prefix('expense')->group(function() {
+Route::middleware('auth:sanctum')->prefix('expense')->group(function() {
     Route::get('/list', 'api\ExpenseController@list');
     Route::post('/store', 'api\ExpenseController@store');
     Route::patch('/update/{category}', 'api\ExpenseController@update');
